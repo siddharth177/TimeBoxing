550614f0c967fe195531e10dec312adfd6829dca
Siddharth <siddharth.dev177@gmail.com>
2026-08-03 20:33:13 +0530

    import settings provider in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
+import 'features/settings/providers/settings_provider.dart';
 
 class TimeBoxApp extends ConsumerWidget {
   const TimeBoxApp({super.key});
