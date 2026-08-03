84439aa49d8adae14e522e67b555766aa8e7e40b
Siddharth <siddharth.dev177@gmail.com>
2026-08-04 00:26:55 +0530

    deploying web UI

--- android/app/src/main/kotlin/com/example/timeboxing/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.timeboxing
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/features/onboarding/widgets/onboarding_illustrations.dart
+++
@@ -1,8 +0,0 @@
-export 'illustrations/time_grid_illustration.dart';
-export 'illustrations/timeline_illustration.dart';
-export 'illustrations/focus_card_illustration.dart';
-export 'illustrations/day_ring_illustration.dart';
-export 'illustrations/goals_illustration.dart';
-export 'illustrations/backlog_illustration.dart';
-export 'illustrations/review_illustration.dart';
-export 'illustrations/ai_goal_illustration.dart';
