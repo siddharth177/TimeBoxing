a0799e609e417afe7d00c7d1f6665406fedbd79d
Siddharth <siddharth.dev177@gmail.com>
2026-08-03 21:20:52 +0530

    adding insight and reflection analysis providers

---
+++ lib/features/review/providers/insight_provider.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../core/providers/firebase_providers.dart';
+import '../models/insight.dart';
+
+final insightsProvider = StreamProvider<List<Insight>>((ref) {
+  final user = ref.watch(currentUserProvider);
+  if (user == null) return Stream.value([]);
+  return FirebaseFirestore.instance
+      .collection('users')
+      .doc(user.uid)
+      .collection('insights')
+      .orderBy('createdAt', descending: true)
+      .limit(5)
+      .snapshots()
+      .map((s) => s.docs.map((d) => Insight.fromDoc(d)).toList());
+});
+
+final reflectionAnalysisServiceProvider =
+Provider<ReflectionAnalysisService?>((ref) {
+  final user = ref.watch(currentUserProvider);
+  if (user == null) return null;
+  return ReflectionAnalysisService(user.uid);
+});
